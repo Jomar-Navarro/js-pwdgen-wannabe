b7185209d8bcd71be597d47c73ba2939183c63c6
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-02-20 09:55:29 +0100

    [add] corrections

--- js/script.js
+++ js/script.js
@@ -12,17 +12,14 @@ const favColor = prompt('Qual é il tuo colore preferito?');
 console.log(favColor);
 
 //Anno attuale
-const actualYear = 2024;
-console.log(actualYear);
+const year = 24;
+console.log(year);
+
+const password = firstName + lastName + favColor + year;
+console.log(password);
 
 //Stampo in HTML
-document.getElementById('messaggio').innerHTML = 
-`
-<p> 
-Ciao <strong> ${fullName}!</strong>, il tuo colore preferito é <strong>${favColor}</strong> e siamo nell'anno <strong>${actualYear}</strong>.
-</p>
-
-`
+document.getElementById('output').innerHTML += ' ' + password;
 
 
 
